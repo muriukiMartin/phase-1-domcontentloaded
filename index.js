3eaaf7ca58a0cd0b9303fa251ce94a1c4004d3f6
Martin Muriuki <helheim254@gmail.com>
2023-06-19 12:32:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    let changeText = document.querySelector("p");
+    changeText.innerHTML = "This is really cool!";
+    return changeText;
+});
+
+// document.addEventListener("DOMContentLoaded",
+//     function() {
+
+//         let p = document.querySelector('p')
+//         p.textContent = ('This is really cool!')
+
+
+//     });
